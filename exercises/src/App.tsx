792922ab19343a6c1edb2fe59be5c889c2fa5bb4
Hugo Deiró <hugodeiro@gmail.com>
2023-07-10 10:56:03 -0300

    Create Title Component with Styles

--- exercises/src/App.tsx
+++ exercises/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import {Title} from './components/Title';
 
 export default () => (
   <SafeAreaView>
-    <Text>My App!</Text>
+    <Title content="My App" />
   </SafeAreaView>
 );
---
+++ exercises/src/components/Title.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+type PropsType = {
+  content: string;
+};
+
+const Styles = StyleSheet.create({
+  Title: {
+    fontWeight: '600',
+    fontSize: 32,
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+});
+
+export const Title = ({content}: PropsType) => {
+  return <Text style={Styles.Title}>{content}</Text>;
+};
